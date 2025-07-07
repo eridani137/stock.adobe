3cff0fc481cc762ea22c7cd016501744b069cc55
Eridani 137 <tsuuzetsu@gmail.com>
2025-07-07 15:54:42 +0300

    🛠️ fix(main.py, config.py): Handle duplicates, increase next page retries, update OS support

--- config.py
+++ config.py
@@ -1,23 +1,13 @@
-import os
 from urllib.parse import urlparse
 
 from browserforge.fingerprints import Screen
 
-PROFILES_DIR = "Profiles"
-PROFILE_DIR = "NotBot"
-profile_path = os.path.abspath(os.path.join(PROFILES_DIR, PROFILE_DIR))
-os.makedirs(profile_path, exist_ok=True)
-
 BROWSER_OPTIONS = {
     "headless": False,
-    "os": ["windows"],
+    "os": ["windows", "macos", "linux"],
     "screen": Screen(max_width=1280, max_height=720),
     "humanize": True,
-    "enable_cache": False,
-    "persistent_context": True,
-    "user_data_dir": profile_path,
-    "locale": "en-US",
-    # "geoip": True,
+    "locale": "en-US"
 }
 
 
--- main.py
+++ main.py
@@ -44,6 +44,8 @@ async def main():
     passed_pages = 0
     index = 1
     browser = None
+    seen_prompts = set()
+    is_complete = False
 
     try:
         with open(file_path, 'w', newline='', encoding='utf-8') as file:
@@ -54,10 +56,13 @@ async def main():
                     **config.BROWSER_OPTIONS
             ) as browser_instance:
                 browser = browser_instance
-                main_page = browser.pages[0]
-                await main_page.goto(url, wait_until="networkidle")
+                main_page = await browser.new_page()
+                await main_page.goto(url)
 
                 while True:
+
+                    await asyncio.sleep(2)
+
                     images = await main_page.query_selector_all(
                         "xpath=//div[@id='search-results']/div//meta[@itemprop='name']")
 
@@ -69,6 +74,11 @@ async def main():
                                 image_name_stripped = image_name.replace('\n', ' ').strip()
                                 image_name_stripped = re.sub(r'\s+', ' ', image_name_stripped).strip()
 
+                                if image_name_stripped in seen_prompts:
+                                    logger.warning(f"Пропущен дубликат: {image_name_stripped}")
+                                    continue
+
+                                seen_prompts.add(image_name_stripped)
                                 writer.writerow([index, image_name_stripped])
                                 logger.info(f"[{index}] {image_name_stripped}")
                                 index += 1
@@ -83,17 +93,20 @@ async def main():
                         logger.warning(f"Достигли заданное количество страниц")
                         break
 
-                    max_retries = 3
+                    max_retries = 10
                     next_page_clicked = False
 
                     for retry in range(max_retries):
                         try:
-                            await main_page.wait_for_timeout(1000)
+                            await asyncio.sleep(2)
 
                             next_page = await main_page.query_selector(
                                 "xpath=//div[@id='pagination-element']/nav/span[last()]/button")
 
                             if next_page:
+                                if await next_page.is_disabled():
+                                    is_complete = True
+                                    break
                                 is_attached = await next_page.evaluate("el => el.isConnected")
                                 if not is_attached:
                                     logger.warning(f"Элемент кнопки не прикреплен к DOM, повторная попытка {retry + 1}")
@@ -105,8 +118,6 @@ async def main():
                                 await next_page_locator.scroll_into_view_if_needed()
                                 await next_page_locator.click()
 
-                                await main_page.wait_for_load_state("networkidle")
-
                                 next_page_clicked = True
                                 break
                             else:
@@ -117,17 +128,21 @@ async def main():
                             logger.warning(
                                 f"Ошибка при попытке перейти на следующую страницу (попытка {retry + 1}): {e}")
                             if retry < max_retries - 1:
-                                await main_page.wait_for_timeout(2000)
+                                await asyncio.sleep(2)
                                 continue
                             else:
                                 logger.error("Не удалось перейти на следующую страницу после всех попыток")
                                 break
 
+                    if is_complete:
+                        logger.info("Достигнута последняя доступная страница")
+                        break
+
                     if not next_page_clicked:
                         logger.warning("Не удалось найти или нажать кнопку следующей страницы")
                         break
 
-                    await main_page.wait_for_timeout(5000)
+                    await asyncio.sleep(7)
 
                 logger.warning(f"Пройдено {count} страниц(ы), завершаем работу")
                 await browser.close()
